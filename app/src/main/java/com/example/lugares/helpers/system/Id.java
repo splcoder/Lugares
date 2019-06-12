7b96eec3d742d95130b2cbbd3889f32d280b0d84
splcoder <www3.splcoder@gmail.com>
2019-06-12 21:17:58 +0200

    falta un poco más...

--- app/src/main/java/com/example/lugares/helpers/system/Id.java
+++ app/src/main/java/com/example/lugares/helpers/system/Id.java
@@ -2,6 +2,7 @@ package com.example.lugares.helpers.system;
 
 import java.io.Serializable;
 
+// See: https://www.dev2qa.com/android-get-application-context-from-anywhere-example/
 public class Id implements Serializable {
 	protected long id = 0;
 	public Id(){}
